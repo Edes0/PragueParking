fe6623f81e8310077dcc6183130b8b87527a81ba
sjogr_v8becyz <sjogr_v8becyz@DESKTOP-91V6B3T>
2021-11-18 11:33:04 +0100

    Fungerar

--- PragueParking2.0/ParkingSpot.cs
+++ PragueParking2.0/ParkingSpot.cs
@@ -23,20 +23,24 @@ namespace PragueParking2._0
         public ParkingSpot()
         {
             if (Number <= Settings.SizeParkingHouseHighRoof)
-                Height = Settings.HightParkingHigh;
-            else Height = Settings.HightParkingLow;
+                Height = Settings.HeightParkingHigh;
+            else Height = Settings.HeightParkingLow;
         }
         internal ParkingSpot(byte aNumber, byte highRoof)
         {
             Number = aNumber;
 
-            if (Number <= highRoof) Height = Settings.HightParkingHigh;
-            else Height = Settings.HightParkingLow;
+            if (Number <= highRoof) Height = Settings.HeightParkingHigh;
+            else Height = Settings.HeightParkingLow;
         }
         public override string ToString()
         {
             return $"ParkingSpot[{Number}]";
         }
+        /// <summary>
+        /// Adds vehicle in parkingspot
+        /// </summary>
+        /// <param name="vehicle"></param>
         internal void AddVehicle(Vehicle vehicle)
         {
             vehicle.Park(this);
@@ -44,6 +48,10 @@ namespace PragueParking2._0
             if (vehicle.IsBig()) AvailableSize -= Size;
             if (vehicle.IsSmall() || vehicle.IsTiny()) AvailableSize -= vehicle.Size;
         }
+        /// <summary>
+        /// Removes vehicle in parkingspot
+        /// </summary>
+        /// <param name="vehicle"></param>
         internal void RemoveVehicle(Vehicle vehicle)
         {
             vehicle.CheckOut(this);
@@ -51,15 +59,28 @@ namespace PragueParking2._0
             if (vehicle.IsSmall() || vehicle.IsTiny()) AvailableSize += vehicle.Size;
             if (vehicle.IsBig()) AvailableSize += Size;
         }
+        /// <summary>
+        /// Clear parking
+        /// </summary>
         internal void Clear()
         {
             VehicleList.Clear();
             AvailableSize = Size;
         }
+        /// <summary>
+        /// Reserve parking
+        /// </summary>
+        /// <param name="parkingSpot"></param>
         internal void Reserve(ParkingSpot parkingSpot)
         {
             parkingSpot.AvailableSize -= Size;
         }
+        /// <summary>
+        /// Checks if vehicle is in parkingspot
+        /// </summary>
+        /// <param name="aRegistrationNumber"></param>
+        /// <param name="aVehicle"></param>
+        /// <returns></returns>
         internal bool VehicleExist(string aRegistrationNumber, out Vehicle aVehicle)
         {
             foreach (Vehicle vehicle in VehicleList)
@@ -73,12 +94,23 @@ namespace PragueParking2._0
             aVehicle = null;
             return false;
         }
+        /// <summary>
+        /// Check if parkingspot is available for parking
+        /// </summary>
+        /// <param name="vehicle"></param>
+        /// <returns></returns>
         internal bool ParkingSpotAvailable(Vehicle vehicle)
         {
-            if (!vehicle.IsBig() && vehicle.FitSize(this) && vehicle.FitHight(this)) return true;
-            if (vehicle.IsBig() && IsFree() && vehicle.FitHight(this)) return true;
+            if (!vehicle.IsBig() && vehicle.FitSize(this) && vehicle.FitHeight(this)) return true;
+            if (vehicle.IsBig() && IsFree() && vehicle.FitHeight(this)) return true;
             return false;
         }
+        /// <summary>
+        /// Gets info from parkingspot to print
+        /// </summary>
+        /// <param name="parkingSpot"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         internal string GetparkingSpotInfo(ParkingSpot parkingSpot)
         {
             var availableSize = $"{parkingSpot.AvailableSize}";
@@ -93,37 +125,65 @@ namespace PragueParking2._0
 
             //if (VehicleList.Count == 0 && parkingSpot.IsFull()) return $"[b]{"RES "}[/]\n[maroon]{parkingSpotNumber}[/]"; // If you want to show reserved spots
         }
+        /// <summary>
+        /// Checks if parking is free
+        /// </summary>
+        /// <returns></returns>
         internal bool IsFree()
         {
             if (AvailableSize == Size) return true;
             return false;
         }
+        /// <summary>
+        /// Checks if parking is reserved
+        /// </summary>
+        /// <returns></returns>
         internal bool IsReserved()
         {
             if (AvailableSize == 0 && VehicleList.Count() == 0) return true;
             return false;
         }
+        /// <summary>
+        /// Checks if parking is full
+        /// </summary>
+        /// <returns></returns>
         internal bool IsFull()
         {
             if (AvailableSize == 0) return true;
             return false;
         }
+        /// <summary>
+        /// Checks if parking is high
+        /// </summary>
+        /// <returns></returns>
         internal bool IsHigh()
         {
-            if (Height >= Settings.HightParkingHigh) return true;
+            if (Height >= Settings.HeightParkingHigh) return true;
             return false;
         }
+        /// <summary>
+        /// Check if parking is low
+        /// </summary>
+        /// <returns></returns>
         internal bool IsLow()
         {
-            if (Height <= Settings.HightParkingLow) return true;
+            if (Height <= Settings.HeightParkingLow) return true;
             return false;
         }
+        /// <summary>
+        /// Gets ticket info at parking
+        /// </summary>
+        /// <returns></returns>
         internal string GetTicketInfo()
         {
             foreach (Vehicle vehicle in VehicleList)
                 return vehicle.GetVehicleInfo();
             return "----";
         }
+        /// <summary>
+        /// Gets vehicle prices from parking
+        /// </summary>
+        /// <returns></returns>
         internal decimal GetVehiclePrices()
         {
             decimal totalPrice = 0;
@@ -134,12 +194,21 @@ namespace PragueParking2._0
             }
             return totalPrice;
         }
+        /// <summary>
+        /// Checks if parking have high vehicle
+        /// </summary>
+        /// <returns></returns>
         internal bool HaveHighVehicle()
         {
             foreach (Vehicle vehicle in VehicleList)
                 if (vehicle.IsHigh()) return true;
             return false;
         }
+        /// <summary>
+        /// Checks if parkingspot is possible to shrink size
+        /// </summary>
+        /// <param name="newSize"></param>
+        /// <returns></returns>
         internal bool PossibleToShrink(byte newSize)
         {
             if (newSize < Size - AvailableSize) return false;
